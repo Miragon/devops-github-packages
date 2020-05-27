4536c235d2cb4451fd5ee6fd0a55c36ca82a94ec
Alexander Praschek <alexander.praschek@flowsquad.io>
2020-05-27 10:43:01 +0200

    Add code

---
+++ devops-github-packages-library/src/main/java/io/flowsquad/blog/devops/github/packages/library/LibraryClass.java
@@ -0,0 +1,7 @@
+package io.flowsquad.blog.devops.github.packages.library;
+
+public class LibraryClass {
+    public String utilityMethod() {
+        return "Hi, I'm a very expensive library method and do a lot of heavy calculations!";
+    }
+}
---
+++ devops-github-packages-main/src/main/java/io/flowsquad/blog/devops/github/packages/main/MainClass.java
@@ -0,0 +1,9 @@
+package io.flowsquad.blog.devops.github.packages.main;
+
+import io.flowsquad.blog.devops.github.packages.library.LibraryClass;
+
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println(new LibraryClass().utilityMethod());
+    }
+}
